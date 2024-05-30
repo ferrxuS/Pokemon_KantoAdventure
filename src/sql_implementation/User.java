92a886c744abe89e5287fb865998fb1ebf3a5f19
ferrxuS <ferashamidbanday22@gmail.com>
2024-05-30 09:20:28 +0800

    added sql_implementation package and necessary lib

---
+++ src/sql_implementation/User.java
@@ -0,0 +1,21 @@
+package sql_implementation;
+
+public class User {
+
+    private int account_id;
+    private String username;
+
+    public User(int account_id, String username) {
+        this.account_id = account_id;
+        this.username = username;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+}
